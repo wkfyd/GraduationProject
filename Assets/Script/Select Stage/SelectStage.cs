eacb2ad3c4cb6c6ef3dab0fe5d6be5d57285a914
wkfyd <106089929+wkfyd@users.noreply.github.com>
2022-10-13 22:19:28 +0900

    10_13

---
+++ Assets/Script/Select Stage/SelectStage.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectStage : MonoBehaviour
+{
+    private GameObject Stage;
+    public void Stage01()
+    {
+        Stage = GameObject.Find("Canvas").transform.Find("Stage01_PopUp").gameObject;
+        Stage.SetActive(true);
+    }
+
+    public void Stage02()
+    {
+        Stage = GameObject.Find("Canvas").transform.Find("Stage02_PopUp").gameObject;
+        Stage.SetActive(true);
+    }
+
+    public void Stage03()
+    {
+        Stage = GameObject.Find("Canvas").transform.Find("Stage03_PopUp").gameObject;
+        Stage.SetActive(true);
+    }
+
+    public void Stage04()
+    {
+        Stage = GameObject.Find("Canvas").transform.Find("Stage04_PopUp").gameObject;
+        Stage.SetActive(true);
+    }
+
+    public void Stage05()
+    {
+        Stage = GameObject.Find("Canvas").transform.Find("Stage05_PopUp").gameObject;
+        Stage.SetActive(true);
+    }
+
+    public void Stage06()
+    {
+        Stage = GameObject.Find("Canvas").transform.Find("Stage06_PopUp").gameObject;
+        Stage.SetActive(true);
+    }
+    public void FreeMode()
+    {
+        Stage = GameObject.Find("Canvas").transform.Find("FreeMode").gameObject;
+        Stage.SetActive(true);
+    }
+}
