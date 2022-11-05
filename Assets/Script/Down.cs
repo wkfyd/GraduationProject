5e0b31f9fdc06de843965ee7ecdc972d98cefde4
wkfyd <106089929+wkfyd@users.noreply.github.com>
2022-11-06 04:46:31 +0900

    11-06_2

---
+++ Assets/Script/Down.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Down : MonoBehaviour
+{
+
+    public void setActive()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
