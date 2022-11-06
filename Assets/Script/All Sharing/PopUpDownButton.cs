cb73d39002c2d839dd73c108b0495384eef46996
wkfyd <106089929+wkfyd@users.noreply.github.com>
2022-11-07 00:50:57 +0900

    11_07

--- Assets/Script/All Sharing/PopUpDownButton.cs
+++ Assets/Script/All Sharing/PopUpDownButton.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PopUpDownButton : MonoBehaviour
 {
     public GameObject PopUp; //게임오브젝트 변수 선언
-    public void NoButtonDown() //On Click() 함수
+    public void PopUpButtonDown() //On Click() 함수
     {
         PopUp.SetActive(false); //startPopUp오브젝트 비활성화
     }
