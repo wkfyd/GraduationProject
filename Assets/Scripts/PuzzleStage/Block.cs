35183fee85979403387cd13a7282ea10ad6e3552
wkfyd <106089929+wkfyd@users.noreply.github.com>
2023-01-04 16:50:35 +0900

    23_01_04

--- Assets/Scripts/PuzzleStage/Block.cs
+++ Assets/Scripts/PuzzleStage/Block.cs
@@ -4,27 +4,22 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    public bool isec = false;
     public bool isDrag;
     Rigidbody2D rigid;
     Vector3 mousePos;
 
-    void Awake()
+   /* void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-    }
+    }*/
 
     void Update()
     {
-        if(isDrag)
-        {
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //월드좌표 마우스 위치
-            mousePos.z = 0;
-            transform.position = Vector3.Lerp(transform.position, mousePos, 0.1f);
-        }
         
     }
 
-    public void Drag()
+    /*public void Drag()
     {
             isDrag = true;
             rigid.simulated = false;
@@ -34,5 +29,20 @@ public class Block : MonoBehaviour
     {
         isDrag = false;
         rigid.simulated = true;
+    }*/
+
+    public void OnMouseDown()
+    {
+        isec = true;
+    }
+    public void OnMouseDrag()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //월드좌표 마우스 위치
+        mousePos.z = 0;
+        transform.position = Vector3.Lerp(transform.position, mousePos, 0.1f);
+    }
+    public void OnMouseUp()
+    {
+        isec = false;
     }
 }
