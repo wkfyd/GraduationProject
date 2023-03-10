fbb3faa789c9f5e98b45db3afc8ab585ce930d57
wkfyd <wkfyd8841@naver.com>
2023-03-10 21:20:06 +0900

    23-03-10

--- Assets/Scripts/PuzzleStage/Block.cs
+++ Assets/Scripts/PuzzleStage/Block.cs
@@ -44,7 +44,7 @@ public class Block : MonoBehaviour
     public void OnMouseUp()
     {
         //BlockGroup중에 몇 번째에 가까운지
-        float snapPos, snapPos1, result;
+        float snapPos, snapPos1, temp;
         int i = 0;
         while (i < 5)
         {
@@ -52,13 +52,17 @@ public class Block : MonoBehaviour
             snapPos1 = transform.position.x - snap.BlockGroupPos[i+1].x; // -0.6
             if(snapPos < snapPos1)
             {
-                break;
+                temp = snapPos;
+                snapPos = snapPos1;
+                snapPos1 = temp;
             }
+            i++;
         }
+        
 
         snapPos1 = transform.position.x - snap.BlockGroupPos[1].x;//이게 가장 적은 걸 찾아야 돼 
 
-        Debug.Log(snap.BlockGroupPos[0].x);
+        Debug.Log(snap.BlockGroupPos[0].x + " " + snapPos1);
 
         select = false;
         rigid.simulated = true;
