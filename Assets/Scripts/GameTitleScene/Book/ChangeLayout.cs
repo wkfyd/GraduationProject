b6193a3fbf043644a1ff1f0960fcba60692e15d3
wkfyd <106089929+wkfyd@users.noreply.github.com>
2023-05-19 04:31:47 +0900

    0519

---
+++ Assets/Scripts/GameTitleScene/Book/ChangeLayout.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLayout : MonoBehaviour
+{
+    public int currentPage;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
